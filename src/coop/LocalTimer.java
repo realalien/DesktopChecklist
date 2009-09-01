f5dca8fadba265bb62622887d20614e42beb5195
Zhu Jia Cheng <realalien@gmail.com>
2009-09-01 17:42:49 +0800

    missing commit

---
+++ src/coop/LocalTimer.java
@@ -0,0 +1,18 @@
+package coop;
+
+/**
+ * External tools used for auto submit source code at the end of each working day, avoiding unsync code 
+ * 
+ * 
+ * */
+public class LocalTimer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
